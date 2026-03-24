311b27d2ccbc1434e530e889fd886daf723e8fd4
jastigi <jastigi@gmail.com>
2026-03-24 12:32:11 +0100

    Componente Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent }
+    { path: '', component: HomeComponent }
 ];
